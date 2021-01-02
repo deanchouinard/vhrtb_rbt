9c98cd44a325e04cd5181933ab170fc363887ab6
Dean Chouinard <deanchouinard@gmail.com>
2021-01-02 13:03:01 -0500

    added python_scripts directory

--- lib/vhrtb_rbt/vhr_port.ex
+++ lib/vhrtb_rbt/vhr_port.ex
@@ -10,5 +10,30 @@ defmodule VhrPort do
     #send(port, {self(), :close})
 
   end
+
+  def add() do
+
+    # add = fn port, nums ->
+    nums = [1,2,3,4,5]
+    port = Port.open({:spawn, "python3 add.py"}, [:binary])
+    # integers to a string
+    msg =
+      nums
+      |> Enum.map(&to_string/1)
+      |> Enum.join(",")
+
+    #  sending the msg and ending "\n" as iolist
+    Port.command(port, [msg, "\n"])
+
+    # receive the result and convert to a string
+    receive do
+      {^port, {:data, result}} ->
+        String.trim(result)
+        |> String.to_integer()
+    end
+    #send(port, {self(), :close})
+  end
+
+
 end
 
